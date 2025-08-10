ee468b15ee078a13dfeb5ff920045b33ee0be13d
Francis Fu <mail@francisfu.com>
2025-08-10 15:50:45 +0800

    feat: 合并前端路由

--- apps/web-fsa/src/router/routes/modules/home.ts
+++ apps/web-fsa/src/router/routes/modules/home.ts
@@ -1,5 +1,6 @@
 import type { RouteRecordRaw } from 'vue-router';
 
+import { IFrameView } from '#/layouts';
 import { $t } from '#/locales';
 
 const routes: RouteRecordRaw[] = [
@@ -67,6 +68,56 @@ const routes: RouteRecordRaw[] = [
           authority: ['ADMIN'],
           icon: 'lucide:database',
           title: $t('page.home.lorealDatabase'),
+        },
+      },
+      {
+        name: 'LorealOfficial',
+        path: '/home/loreal-official',
+        component: IFrameView,
+        meta: {
+          icon: 'mdi:lipstick',
+          link: 'https://www.loreal-boutique.com/',
+          title: $t('page.home.lorealOfficial'),
+        },
+      },
+      {
+        name: 'EsteeOfficial',
+        path: '/home/estee-official',
+        component: IFrameView,
+        meta: {
+          icon: 'mdi:flower-outline',
+          link: 'https://elc.vipsale.cn/',
+          title: $t('page.home.esteeOfficial'),
+        },
+      },
+      {
+        name: 'LoccitaneOfficial',
+        path: '/home/loccitane-official',
+        component: IFrameView,
+        meta: {
+          icon: 'lucide:sparkles',
+          link: 'https://loccitane.vipsale.cn/',
+          title: $t('page.home.loccitaneOfficial'),
+        },
+      },
+      {
+        name: 'Analytics',
+        path: '/home/analytics',
+        component: () => import('#/views/dashboard/analytics/index.vue'),
+        meta: {
+          authority: ['ADMIN'],
+          icon: 'lucide:area-chart',
+          title: $t('page.home.analytics'),
+        },
+      },
+      {
+        name: 'About',
+        path: '/home/about',
+        component: () => import('#/views/_core/about/index.vue'),
+        meta: {
+          authority: ['ADMIN'],
+          icon: 'lucide:copyright',
+          title: $t('page.home.about'),
         },
       },
     ],
